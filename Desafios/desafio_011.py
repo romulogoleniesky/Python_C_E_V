e4afa71082e33404ae95ce8efb68c47b12a4ffa5
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-21 12:07:38 -0300

    desafio 11 finalizado

---
+++ Desafios/desafio_011.py
@@ -0,0 +1,4 @@
+l = float(input('Qual é a Largura da parede?'))
+a = float(input('Qual é a Altura da parede?'))
+print(f'Sua parede tem a dimensão de {l} X {a} e sua área é de {l*a:.2f}m².')
+print(f'Para pintar esta parede você precisará de {(l*a)/2:.2f} L de tinta.')
