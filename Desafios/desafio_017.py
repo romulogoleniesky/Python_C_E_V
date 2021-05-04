de41af2c9c59f7e5431725f7419c81d0a68e7519
RomuloGoleniesky <goleniesky@hotmail.com>
2021-05-04 13:18:32 -0300

    criado o desafio 17

---
+++ Desafios/desafio_017.py
@@ -0,0 +1,11 @@
+# Trigonometria com python
+import math
+CO= float(input('Comprimento do cateto oposto: '))
+CA= float(input('Comprimento do cateto adjacente: '))
+hip= math.sqrt(CO**2+CA**2)
+print(f'A hipotenusa vai medir : {hip:.2f}')
+
+CO= float(input('Comprimento do cateto oposto: '))
+CA= float(input('Comprimento do cateto adjacente: '))
+hi= math.hypot(CO, CA)
+print(f'A hipotenusa vai medir : {hi:.2f}')
