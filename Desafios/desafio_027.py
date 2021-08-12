bddc1c3d5e3c8cc430a7af3e670465ede2896d4c
RomuloGoleniesky <goleniesky@hotmail.com>
2021-08-12 10:58:16 -0300

    Desafio 27 concluído!

---
+++ Desafios/desafio_027.py
@@ -0,0 +1,7 @@
+#DESAFIO 27:
+
+n = input('Escreva seu nome completo: ').strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print(f'Seu primeiro nome é {nome[0]}.')
+print(f'Seu último nome é {nome[-1]}.')
