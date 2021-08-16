095926a0699303c51a56d4844931b2b7ffecca45
RomuloGoleniesky <goleniesky@hotmail.com>
2021-08-16 00:26:56 -0300

    Criado o desafio 29.

---
+++ Desafios/desafio_029.py
@@ -0,0 +1,15 @@
+#DESAFIO 29
+
+import time
+
+vel = int(input('Qual a velocidade do seu carro? '))
+with open("texto.txt") as file:
+    texto = file.read().splitlines()
+    for linha in texto:
+        print(linha)
+        time.sleep(1)
+if vel <= 80:
+    print('OK, velocidade dentro do permitido, siga sua viagem.')
+else:
+    print(f'Você ultrapassou a velocidade permitida de 80km/h, paragá multa de R${(vel-80)*7.00:.2f}. ')
+
