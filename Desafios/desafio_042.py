2dcf5fb3505a20443788a284c52114c6434118ce
RomuloGoleniesky <goleniesky@hotmail.com>
2022-01-04 00:17:30 -0300

    Desafio 42 - concluído.

--- Desafios/desafio_042.py
+++ Desafios/desafio_042.py
@@ -1,4 +1,4 @@
-# DESAFIO 35 - ANALISADOR DE TRIANGULOS:
+# DESAFIO 42 - ANALISADOR DE TRIANGULOS-parte 2:
 import time
 print("=-"*30)
 print("              ANALISADOR DE TRIÂNGULOS ")
@@ -9,9 +9,16 @@ c = float(input("Entre com o terceiro segmento de reta: "))
 print("Analisando os segmentos...")
 time.sleep(2)
 if a+b > c and b+c > a and a+c > b:
-    res = "FORMAM"
+    res = "FORMAM UM TRIÂNGULO"
+    if a == b == c:
+        res = "FORMAM UM TRIÂNGULO EQUILÁTERO"
+    elif a != b != c != a:
+        res = "FORMAM UM TRIÂNGULO ESCALENO"
+    else:
+        res = "FORMAM UM TRIÂNGULO ISOCELES"
+
 else:
-    res = "NÃO FORMAM"
+    res = "NÃO FORMAM UM TRIÂNGULO."
 print("=-"*30)
-print(f"OS SEGMENTOS {res} UM TRIÂNGULO.")
+print(f"OS SEGMENTOS {res}.")
 print("=-"*14, "FIM", "=-"*14)
