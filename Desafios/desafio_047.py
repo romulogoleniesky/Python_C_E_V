694e37e8eed25ebf84205286e9829b7d0886ff3c
RomuloGoleniesky <goleniesky@hotmail.com>
2022-03-29 15:45:14 -0300

    Desafio - 047 Concluído

---
+++ Desafios/desafio_047.py
@@ -0,0 +1,9 @@
+# DESAFIO 047: Mostrar somente os números pares de 2 a 50.
+#vertical
+for i in range(2, 51, 2):
+    print(i)
+print("fim")
+#horizontal
+for a in range(2, 51, 2):
+    print(a, end=" ")
+
