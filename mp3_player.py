ef372579693566a531a71f49249cb776e5f64953
RomuloGoleniesky <goleniesky@hotmail.com>
2023-07-29 18:33:41 -0300

    Update and deletes!

--- mp3_player.py
+++
@@ -1,6 +0,0 @@
-import pygame
-pygame.init()
-
-som = pygame.mixer.Sound('mission.wav')
-som.play(-1)
-som.set_volume(1)
--- tocamusica.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.mixer.music.load('Masked Wolf - Astronaut in the Ocean.mp3')
-pygame.mixer.music.play()
-pygame.event.wait(10)
